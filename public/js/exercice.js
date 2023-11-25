421a34c1dac1bb22f211122c916363f59af108f6
Lukam90 <lucien.hamm@gmail.com>
2023-11-25 13:08:56 +0100

    MAJ Nv Exercice

---
+++ public/js/exercice.js
@@ -0,0 +1,41 @@
+const MAX_TIME = 300;
+const NOTICE = " PAR LE NEZ (5 SECONDES)";
+
+let counter = MAX_TIME;
+let started = false;
+let timer;
+
+const elCounter = document.getElementById("counter");
+const elStep = document.getElementById("step");
+const btnPress = document.getElementById("press_btn");
+
+function myTimeout() {
+    if (counter < 0) {
+        counter = MAX_TIME;
+        started = false;
+
+        btnPress.innerText = "COMMENCER"
+        elStep.innerText = "";
+
+        clearInterval(timer);
+    }
+
+    if (counter % 5 == 0 && counter % 10 == 0)  elStep.innerText = "INSPIRER" + NOTICE;
+    if (counter % 5 == 0 && counter % 10 != 0)  elStep.innerText = "EXPIRER" + NOTICE;
+
+    elCounter.innerText = counter--;
+}
+
+function setCounter() {
+    if (started) {
+        clearInterval(timer);
+
+        btnPress.innerText = "RELANCER";
+    } else {
+        timer = setInterval(myTimeout, 1000);
+
+        btnPress.innerText = "PAUSE";
+    }
+
+    started = ! started;
+}
