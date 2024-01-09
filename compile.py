697b2b43bcd4a607cdf551622e2034fe25e43440
Lukam90 <lucien.hamm@gmail.com>
2024-01-09 22:25:27 +0100

    MAJ R Jinja > CV & Exercices

---
+++ compile.py
@@ -0,0 +1,33 @@
+#coding: utf8
+
+from jinja2 import Environment, FileSystemLoader
+
+# Listes
+
+exercices = ["ex-base", "ex-carre", "ex-dormir", "ex-sonore", "ex-video"]
+
+# Conversion Jinja > HTML
+
+def convert_jinja(filename):
+    template_env = Environment(loader = FileSystemLoader(searchpath = "."))
+    template = template_env.get_template(filename + ".jinja")
+
+    with open(filename + ".html", "w") as file:
+        file.write(
+            template.render()
+        )
+
+        file.close()
+
+# Conversion Jinja > Liste
+
+def convert_list(path, files):
+    for file in files:
+        filename = path + "/" + file
+
+        convert_jinja(filename)
+
+#convert_list("exercices", exercices)
+
+#convert_jinja("cv-imprime")
+convert_jinja("portfolio")
