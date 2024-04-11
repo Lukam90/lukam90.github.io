0051dcd34f43034ff7f9e7ea93258ec883b551e1
Lucien HAMM <lucien.hamm@gmail.com>
2024-04-11 14:06:13 +0200

    MAJ R Markdown V1

--- compile.py
+++ compile.py
@@ -1,6 +1,17 @@
 #coding: utf8
 
+import re
+
 from jinja2 import Environment, FileSystemLoader
+
+# Remplacements
+
+replaces = {
+    "**:" : "<b>",
+    ":**" : "</b>",
+    "*:" : "<i>",
+    ":*" : "</i>"
+}
 
 # Listes
 
@@ -27,7 +38,54 @@ def convert_list(path, files):
 
         convert_jinja(filename)
 
-#convert_list("exercices", exercices)
+# Conversion Markdown > HTML
 
-convert_jinja("cv-imprime")
-convert_jinja("portfolio")
+def convert_markdown(filename):
+    filename = "posts/" + filename
+
+    md_content = ""
+    html_content = ""
+
+    # Lecture (MD)
+
+    with open(filename + ".md", "r") as file:
+        lines = file.readlines()
+
+        for line in lines:
+            line = line.strip()
+
+            if line != "":
+                if line[0] == "#":
+                    level = 0
+
+                    while line[level] == "#":
+                        level += 1
+                    
+                    line = line.replace("#", "")
+                    line = line.lstrip()
+
+                    md_content += f"<h{level}>{line}</h{level}>\n"
+                elif line[0] == "<":
+                    md_content += line + "\n"
+                else:
+                    md_content += "<p>" + line + "</p>\n"
+            else:
+                md_content += "\n"
+
+        for key, value in replaces.items():
+            md_content = md_content.replace(key, value)
+
+        html_content = md_content
+
+        file.close()
+
+    # Ecriture (HTML)
+
+    with open(filename + ".html", "w") as file:
+        file.write(html_content)
+        file.close()
+
+convert_markdown("test")
+
+#convert_jinja("cv-imprime")
+#convert_jinja("portfolio")
