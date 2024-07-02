ca7127ee54d818be98451c541ce371a7c8961bf7
Lucien HAMM <lucien.hamm@gmail.com>
2024-07-03 00:27:33 +0200

    MAJ C St Antoine V2

--- public/js/ch-st-antoine.js
+++ public/js/ch-st-antoine.js
@@ -3,26 +3,69 @@ const spanSuite = document.getElementById('suite');
 const selRonde = document.getElementById('sel_ronde');
 
 const suites = [
-    "qui avez ressuscité les morts,\n priez pour ces chrétiens maintenant à l'agonie,\n et pour nos chers défunts",
-    "prédicateur zélé de l'Évangile,\n fortifiez-nous contre les erreurs des ennemis de Dieu,\n et priez pour le Saint-Père et l'Église",
-    "puissant du Cœur de Jésus,\n préservez-nous des calamités qui nous menacent à cause de nos péchés",
-    "qui chassiez les démons,\n faites-nous triompher de leurs pièges",
-    "lys de la pureté céleste,\n purifie-nous des souillures de l'âme\n et préserve notre corps de tous les dangers",
-    "guérisseur des malades,\n guéris nos maladies \n et conserve-nous la santé",
-    "guide des voyageurs,\n ramène à bon port ceux qui sont en danger de périr\n et calme les ondes troubles de la passion qui agitent nos âmes",
-    "libérateur des captifs,\n délivre-nous de la captivité du mal",
-    "qui restituez aux personnes jeunes et âgées l'usage de leurs membres,\n obtenez-nous l'usage parfait des sens de notre corps \n et des facultés de notre âme",
-    "découvreur des choses perdues,\n aide-nous à retrouver tout ce que nous avons perdu \n dans l'ordre spirituel et temporel",
-    "protégé par Marie,\n préviens les dangers qui menacent notre corps et notre âme",
-    "aide les pauvres,\n aide-nous dans nos besoins \n et donne du pain et du travail à ceux qui le demandent",
-    "nous proclamons avec reconnaissance ta puissance miraculeuse,\n et nous te supplions de nous protéger tous les jours de notre vie",
+    // 0 : bref
+    `Voici la croix du Seigneur ! 
+    Fuyez, puissances ennemis ! 
+    Le lion de la tribu de Juda, le rejeton de David, a vaincu ! 
+    Alléluia !`, 
+    // 1
+    `qui avez ressuscité les morts, 
+    priez pour ces chrétiens maintenant à l'agonie, 
+    et pour nos chers défunts.`, 
+    // 2
+    `prédicateur zélé de l'Évangile, 
+    fortifiez-nous contre les erreurs des ennemis de Dieu, 
+    et priez pour le Saint-Père et l'Église.`, 
+    // 3
+    `puissant du Cœur de Jésus, 
+    préservez-nous des calamités qui nous menacent 
+    à cause de nos péchés.`, 
+    // 4
+    `qui chassiez les démons, 
+    faites-nous triompher de leurs pièges.`, 
+    // 5
+    `lys de la pureté céleste, 
+    purifie-nous des souillures de l'âme  
+    et préserve notre corps de tous les dangers.`, 
+    // 6
+    `guérisseur des malades, 
+    guéris nos maladies  
+    et conserve-nous la santé.`, 
+    // 7
+    `guide des voyageurs, 
+    ramène à bon port ceux qui sont en danger de périr  
+    et calme les ondes troubles de la passion qui agitent nos âmes.`, 
+    // 8
+    `libérateur des captifs, 
+    délivre-nous de la captivité du mal.`, 
+    // 9
+    `qui restituez aux personnes jeunes et âgées l'usage de leurs membres, 
+    obtenez-nous l'usage parfait des sens de notre corps  
+    et des facultés de notre âme.`,
+    // 10
+    `découvreur des choses perdues, 
+    aide-nous à retrouver tout ce que nous avons perdu  
+    dans l'ordre spirituel et temporel.`,
+    // 11
+    `protégé par Marie, 
+    préviens les dangers qui menacent notre corps et notre âme.`,
+    // 12
+    `aide les pauvres, 
+    aide-nous dans nos besoins  
+    et donne du pain et du travail à ceux qui le demandent.`,
+    // 13
+    `nous proclamons avec reconnaissance ta puissance miraculeuse, 
+    et nous te supplions de nous protéger tous les jours de notre vie.`,
 ];
 
 let index = 0;
+let suite = "";
 
 function setSuite()
 {
     index = selRonde.selectedIndex;
+    suite = suites[index];
 
-    spanSuite.innerText = suites[index];
+    if (index > 0)  spanSuite.innerText = "Saint Antoine,\n" + suite;
+    else            spanSuite.innerText = suite;
 }
