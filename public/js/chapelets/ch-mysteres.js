f608bf397c2e58f269610b7c2d901a18fe29e019
Lucien HAMM <lucien.hamm@gmail.com>
2024-07-10 00:20:22 +0200

    MAJ R Fusion > Douloureux

--- public/js/chapelets/ch-mysteres.js
+++ public/js/chapelets/ch-mysteres.js
@@ -36,9 +36,12 @@ const m_lumineux = {
 // Mystères douloureux
 
 const m_douloureux = {
-    "mysteres" : [],
-    "fruits" : [],
-    "clausules" : [],
+    "mysteres" : ["1. L'agonie de Jésus", "2. La flagellation", "3. Le couronnement d'épines", "4. Le portement de croix", "5. La crucifixion"],
+    "fruits" : [
+        "le regret de nos péchés", "la maîtrise des sens", "l'humilité et le pardon de nos péchés d'orgueil",
+        "la patience dans les épreuves", "la soif du salut des âmes"
+    ],
+    "clausules" : ["agonisant au jardin de Gethsémani", "flagellé", "couronné d'épines", "portant sa croix jusqu'au Golgotha", "crucifié"],
     "intros" : [
         `
         
