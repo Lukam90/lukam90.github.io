9b0ea954fa7d956a676dfe4f855c3374a368e8fe
Lucien HAMM <lucien.hamm@gmail.com>
2024-07-10 00:08:42 +0200

    MAJ R Fusion > Joyeux

---
+++ public/js/chapelets/ch-mysteres.js
@@ -0,0 +1,60 @@
+// Mystères joyeux
+
+const m_joyeux = {
+    "mysteres" : ["1. L'annonciation", "2. La visitation", "3. La nativité", "4. La présentation de Jésus au temple", "5. Le recouvrement de Jésus au temple"],
+    "fruits" : ["l'humilité", "la charité fraternelle", "l'esprit de pauvreté", "la pureté et l'esprit d'obéissance", "la recherche de Dieu en tout"],
+    "clausules" : ["incarné", "le fruit de vos entrailles", "né à Bethléem", "présenté au temple", "perdu et retrouvé au temple"],
+    "intros" : [
+        `
+        
+        `
+    ]
+};
+
+// Mystères lumineux
+
+const m_lumineux = {
+    "mysteres" : [],
+    "fruits" : [],
+    "clausules" : [],
+    "intros" : [
+        `
+        
+        `
+    ]
+};
+
+// Mystères douloureux
+
+const m_douloureux = {
+    "mysteres" : [],
+    "fruits" : [],
+    "clausules" : [],
+    "intros" : [
+        `
+        
+        `
+    ]
+};
+
+// Mystères glorieux
+
+const m_glorieux = {
+    "mysteres" : [],
+    "fruits" : [],
+    "clausules" : [],
+    "intros" : [
+        `
+        
+        `
+    ]
+};
+
+// Mystères du rosaire
+
+const rosaire = {
+    "joyeux" : m_joyeux,
+    "lumineux" : m_lumineux,
+    "douloureux" : m_douloureux,
+    "glorieux" : m_glorieux
+};
