30363280600119682321d6c946a1e3b66b8ebbe0
Lucien HAMM <lucien.hamm@gmail.com>
2024-07-10 21:00:15 +0200

    MAJ R Lumineux > JSON

--- public/js/chapelets/ch-intros.js
+++ public/js/chapelets/ch-intros.js
@@ -3,27 +3,26 @@
 // Intro > Mystères joyeux
 
 intros_joyeux = [
-    // 1
     `Je t'offre, Seigneur Jésus, cette première dizaine, 
     en l'honneur du mystère de l'annonciation,
     et je te demande, par ce mystère et l'annonciation de ta sainte mère,
     une profonde humilité de coeur.`,
-    // 2
+
     `Je t'offre, Seigneur Jésus, cette dizaine 
     en l'honneur de la visitation de ta sainte mère à sa cousine Ste Elisabeth,
     et je te demande, par ce mystère et l'intercession de Marie,
     une parfaite charité envers mon prochain.`,
-    // 3
+
     `Je t'offre, enfant Jésus, cette dizaine
     en l'honneur de ta nativité,
 	et je te demande, par ce mystère et l'intercession de ta sainte mère,
 	le détachement des biens du monde, l'amour de la pauvreté et des pauvres.`,
-    // 4
+
     `Je t'offre, Seigneur Jésus, cette dizaine 
     en l'honneur de ta présentation au temple,
 	et je te demande, par ce mystère et l'intercession de ta sainte mère,
 	le don de la sagesse et la pureté de coeur et de corps.`,
-    // 5
+
     `Je t'offre, Seigneur Jésus, cette dizaine 
     en l'honneur de ton recouvrement au temple
 	et je te demande, par ce mystère et l'intercession de ta sainte mère,
@@ -33,27 +32,26 @@ intros_joyeux = [
 // Intro > Mystères lumineux
 
 intros_lumineux = [
-    // 1
     `Je t'offre, Seigneur Jésus, cette dizaine 
     en l'honneur de ton baptême dans le Jourdain
     et je te demande, par ce mystère et l'intercession de ta sainte mère,
     une parfaite fidélité aux promesses de mon baptême.`,
-    // 2
+
     `Je t'offre, Seigneur Jésus, cette dizaine 
     en l'honneur des noces de Cana
     et je te demande, par ce mystère et l'intercession de ta sainte mère,
     une parfaite confiance en elle.`,
-    // 3
+
     `Je t'offre, Seigneur Jésus, cette dizaine 
     en l'honneur de l'annonce du Royaume de Dieu,
     et je te demande, par ce mystère et l'intercession de ta sainte mère,
     une parfaite conversion de mon coeur.`,
-    // 4
+
     `Je t'offre, Seigneur Jésus, cette dizaine 
     en l'honneur de ta transfiguration
     et je te demande, par ce mystère et l'intercession de ta sainte mère,
     un plus grand recueillement dans mes prières.`,
-    // 5
+
     `Je t'offre, Seigneur Jésus, cette dizaine 
     en l'honneur de l'institution du sacrement de l'eucharistie,
     et je te demande, par ce mystère et l'intercession de ta sainte mère,
@@ -63,20 +61,19 @@ intros_lumineux = [
 // Intro > Mystères douloureux
 
 intros_douloureux = [
-    // 1
     `Jésus, agonissant au jardin de Gethsémani,
     et pleinement soumis à la volonté du Père,
     nous te louons, te bénissons et t'adorons.`,
-    // 2
+
     `Jésus, cruellement flagellé pour nos péchés de sensualité et d'impureté,
     nous te louons, te bénissons et t'adorons.`,
-    // 3
+
     `Jésus, couronné d'épines et outragé,
     nous te louons, te bénissons et t'adorons.`,
-    // 4
+
     `Jésus, portant sa croix et marchant vers le Golgotha,
     nous te louons, te bénissons et t'adorons.`,
-    // 5
+
     `Jésus, cloué et mort sur la croix, et au coeur transpercé,
     nous te louons, te bénissons et t'adorons.`
 ];
@@ -84,29 +81,28 @@ intros_douloureux = [
 // Intro > Mystères glorieux
 
 intros_glorieux = [
-    // 1
     `Je t'offre, Seigneur Jésus, cette dizaine 
     en l'honneur de ta triomphante ressurection,
     et je te demande, par ce mystère et l'intercession de ta sainte mère, 
     une vive foi.`,
-    // 2
+
     `Je t'offre, Seigneur Jésus, cette dizaine 
     en l'honneur de ta glorieuse ascension,
     et je te demande, par ce mystère et l'intercession de ta sainte mère,
     une ferme espérance et un grand désir du paradis.`,
-    // 3
+
     `Je t'offre, Seigneur Jésus, cette dizaine 
     en l'honneur du mystère de la Pentecôte,
     et je te demande, par ce mystère et l'intercession de Marie,
     la divine sagesse pour connaître, goûter et pratiquer la vérité 
     et la faire participer à tout le monde.`,
-    // 4
+
     `Je t'offre, Seigneur Jésus, cette dizaine 
     en l'honneur de l'Immaculée Conception
     et de l'Assomption en corps et en âme de ta sainte mère dans les cieux,
     et je te demande, par ces deux mystères et son intercession,
     une vraie dévotion envers elle pour bien vivre et bien mourir.`,
-    // 5
+
     `Je t'offre, Seigneur Jésus, 
     par les 20 mystères de ta vie, ta mort et ta passion, 
     ta gloire et les mérites de ta sainte mère, de convertir les pécheurs, 
