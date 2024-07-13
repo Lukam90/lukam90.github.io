d8dabd6616027f02ba09752db17bc23739b6fec1
Lucien HAMM <lucien.hamm@gmail.com>
2024-07-13 23:15:31 +0200

    MAJ R C-Liberation

--- compile.py
+++ compile.py
@@ -167,12 +167,13 @@ profil = ["cv-imprime", "portfolio"]
 
 chapelets = ["chapelet", "c-anges",  "c-eucharistie", "c-liberation", "c-misericorde", "c-sacrement"]
 
-ch_saints = ["c-st-antoine", "c-st-esprit", "c-st-charbel"]
+#ch_saints = ["c-st-antoine", "c-st-esprit", "c-st-charbel"]
 
 #convert_jinja_list("chapelets", chapelets)
 #convert_jinja_list("chapelets/saints", ch_saints)
 
-convert_jinja_single("chapelets/chapelet")
+#convert_jinja_single("chapelets/chapelet")
+convert_jinja_single("chapelets/c-liberation")
 
 ## Exercices
 
