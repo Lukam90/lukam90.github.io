da482b0305639b6d7af6fc8f5c8edbeb95054f9c
Lucien HAMM <lucien.hamm@gmail.com>
2024-07-23 14:45:37 +0200

    MAJ R Intros > Lumineux

--- public/js/data/d.mysteres.js
+++ public/js/data/d.mysteres.js
@@ -2,23 +2,23 @@
 
 const m_joyeux = {
     liste : [
-        "1. L'annonciation", 
-        "2. La visitation", 
-        "3. La nativité", 
-        "4. La présentation de Jésus au temple", 
+        "1. L'annonciation",
+        "2. La visitation",
+        "3. La nativité",
+        "4. La présentation de Jésus au temple",
         "5. Le recouvrement de Jésus au temple"],
     fruits : [
-        "l'humilité", 
-        "la charité fraternelle", 
-        "l'esprit de pauvreté", 
-        "la pureté et l'esprit d'obéissance", 
+        "l'humilité",
+        "la charité fraternelle",
+        "l'esprit de pauvreté",
+        "la pureté et l'esprit d'obéissance",
         "la recherche de Dieu en tout"
     ],
     clausules : [
-        "incarné", 
-        "le fruit de vos entrailles", 
-        "né à Bethléem", 
-        "présenté au temple", 
+        "incarné",
+        "le fruit de vos entrailles",
+        "né à Bethléem",
+        "présenté au temple",
         "perdu et retrouvé au temple"
     ]
 };
@@ -27,24 +27,24 @@ const m_joyeux = {
 
 const m_lumineux = {
     liste : [
-        "1. Le baptême de Jésus", 
-        "2. Les noces de Cana", 
-        "3. L'annonce du royaume", 
-        "4. La transfiguration", 
+        "1. Le baptême de Jésus",
+        "2. Les noces de Cana",
+        "3. L'annonce du Royaume de Dieu",
+        "4. La transfiguration",
         "5. L'institution de l'eucharistie"
     ],
     fruits : [
-        "la fidelité aux promesses de notre baptême", 
-        "la confiance en Marie", 
+        "la fidelité aux promesses de notre baptême",
+        "la confiance en Marie",
         "la conversion des coeurs",
-        "la grâce d'une vie intérieure", 
+        "la grâce d'une vie intérieure",
         "l'amour de l'eucharistie"
     ],
     clausules : [
-        "baptisé dans le Jourdain", 
-        "qui vous exauça à Cana", 
-        "qui annonça la Bonne Nouvelle", 
-        "transfiguré devant les apôtres", 
+        "baptisé dans le Jourdain",
+        "qui vous exauça à Cana",
+        "qui annonça la Bonne Nouvelle",
+        "manifestant sa gloire aux apôtres",
         "qui donna le sacrement de son amour"
     ]
 };
@@ -53,24 +53,24 @@ const m_lumineux = {
 
 const m_douloureux = {
     liste : [
-        "1. L'agonie de Jésus", 
-        "2. La flagellation", 
+        "1. L'agonie de Jésus",
+        "2. La flagellation",
         "3. Le couronnement d'épines",
-        "4. Le portement de croix", 
+        "4. Le portement de croix",
         "5. La crucifixion"
     ],
     fruits : [
-        "le regret de nos péchés", 
-        "la maîtrise des sens", 
+        "le regret de nos péchés",
+        "la maîtrise des sens",
         "l'humilité et le pardon de nos péchés d'orgueil",
-        "la patience dans les épreuves", 
+        "la patience dans les épreuves",
         "la soif du salut des âmes"
     ],
     clausules : [
-        "agonisant au jardin de Gethsémani", 
-        "flagellé", 
-        "couronné d'épines", 
-        "portant sa croix jusqu'au Golgotha", 
+        "agonisant au jardin de Gethsémani",
+        "flagellé",
+        "couronné d'épines",
+        "portant sa croix jusqu'au Golgotha",
         "crucifié"
     ]
 };
@@ -79,24 +79,24 @@ const m_douloureux = {
 
 const m_glorieux = {
     liste : [
-        "1. La résurrection", 
-        "2. L'ascension de Jésus", 
-        "3. La Pentecôte", 
-        "4. L'Assomption de Marie", 
+        "1. La résurrection",
+        "2. L'ascension de Jésus",
+        "3. La Pentecôte",
+        "4. L'Assomption de Marie",
         "5. Le couronnement de Marie"
     ],
     fruits : [
-        "la foi et la conversion des coeurs", 
-        "l'espérance et le désir du ciel", 
-        "la charité et le zèle apostolique", 
-        "la grâce d'une bonne mort et la dévotion filiale à Marie", 
+        "la foi et la conversion des coeurs",
+        "l'espérance et le désir du ciel",
+        "la charité et le zèle apostolique",
+        "la grâce d'une bonne mort et la dévotion filiale à Marie",
         "la persévérance finale et la confiance en Marie"
     ],
     clausules : [
-        "ressuscité", 
-        "monté aux cieux", 
-        "dont le Père envoya le Saint-Esprit sur les apôtres", 
-        "qui vous éleva au ciel", 
+        "ressuscité",
+        "monté aux cieux",
+        "dont le Père envoya le Saint-Esprit sur les apôtres",
+        "qui vous éleva au ciel",
         "qui vous a couronnée reine des anges"
     ]
 };
