9ba2a36341558dd1c8ddad9af439f96d06e26ee6
Lucien HAMM <lucien.hamm@gmail.com>
2024-07-24 15:32:06 +0200

    MAJ C-Liberation > R Clausules

--- public/js/chapelets/ch-liberation.js
+++ public/js/chapelets/ch-liberation.js
@@ -1,19 +1,26 @@
-const blocks = $all(".grains");
+const blocs = $all(".blocs");
 
 const selRondes = $("#sel_rondes");
-
 const btnPremier = $("#dz_grains_1");
+const spanClausule = $("#clausule");
+
+const clausules = ["aie pitié de", "guéris", "sauve", "libère", "aide"];
 
 let index = 0;
 
-switchBlocks("#base_trio");
+switchBlocks("#bloc_trio");
 
 function switchBlocks(id)
 {
-    hideAll(blocks);
+    hideAll(blocs);
     showBlock(id);
 
     checkFirst();
+}
+
+function setClausule(index)
+{
+    spanClausule.textContent = clausules[index];
 }
 
 function setSuite()
@@ -22,10 +29,12 @@ function setSuite()
 
     if (index == 0)
     {
-        switchBlocks("#base_trio");
+        switchBlocks("#bloc_trio");
     }
     else
     {
-        switchBlocks("#dz_grains");
+        switchBlocks("#bloc_dizaine");
+
+        setClausule(index - 1);
     }
 }
--- public/js/latin.js
+++ public/js/latin.js
@@ -1,18 +1,35 @@
-const cbLatin = $("#vlatine");
-
+const tradSpans = $all(".traduction");
 const frBlocks = $all("ul[lang='fr']");
 const latinBlocks = $all("ul[lang='latin']");
 
-function switchLang () 
+let lang;
+
+function setLangSpans()
 {
-    if (cbLatin.checked)
+    for (let span of tradSpans)
+    {
+        span.textContent = lang;
+    }
+}
+
+function switchLang (event) 
+{
+    lang = event.target.textContent;
+
+    if (lang == "Latin")
     {
         hideAll(frBlocks);
         showAll(latinBlocks);
+
+        lang = "FR";
     }
     else
     {
         hideAll(latinBlocks);
         showAll(frBlocks);
+
+        lang = "Latin";
     }
+
+    setLangSpans();
 }
