a65bd804fff7fa19734e482e3ceb4e47b4b57fd1
Lucien HAMM <lucien.hamm@gmail.com>
2024-08-01 20:22:47 +0200

    MAJ R Chapelet > Divisions

--- compile.py
+++ compile.py
@@ -186,15 +186,16 @@ convert_jinja_list("profil", profil)
 
 ## Chapelets
 
-chapelets = ["chapelet", "c-anges", "c-eucharistie", "c-liberation", "c-merci", "c-misericorde", "c-sang"]
+chapelets = ["chapelet", "c-anges", "c-eucharistie", "c-liberation", "c-louanges", "c-merci", "c-misericorde", "c-sang"]
 
 ch_saints = ["c-st-antoine", "c-ste-rita", "c-ste-therese"]
 
 #convert_jinja_list("chapelets", chapelets)
 #convert_jinja_list("chapelets/saints", ch_saints)
 
-#convert_jinja_single("chapelets/chapelet")
-convert_jinja_single("chapelets/c-liberation")
+convert_jinja_single("chapelets/chapelet")
+#convert_jinja_single("chapelets/c-liberation")
+convert_jinja_single("chapelets/c-louanges")
 #convert_jinja_single("chapelets/c-merci")
 #convert_jinja_single("chapelets/c-misericorde")
 #convert_jinja_single("chapelets/c-sang")
