83e87702366ccf8a37c66086955a65e4de5cf39a
Lucien HAMM <lucien.hamm@gmail.com>
2024-08-05 19:14:26 +0200

    MAJ R Versets > Glorieux

--- public/js/chapelets/mysteres/ch-glorieux.js
+++ public/js/chapelets/mysteres/ch-glorieux.js
@@ -24,20 +24,32 @@ const ch_glorieux = {
     ],
     versets : [
         // 1
-        `"Il n’est pas ici, car il est ressuscité, comme il l’avait dit." (Mt 28, 6)`,
+        `L'ange prit la parole et dit aux femmes :
+        "Vous, soyez sans crainte !
+        Je sais que vous cherchez Jésus le crucifié.
+        Il n'est pas ici, car il est ressuscité, comme il l'avait dit.
+        Venez voir l'endroit où il reposait." (Mt 28, 5-6)`,
         // 2
-        `"Ainsi est-il écrit que le Christ souffrirait, 
-        qu’il ressusciterait d’entre les morts le 3ème jour." (Lc 24, 46)`,
-        // 2
-        `Tous furent remplis d’Esprit Saint. 
+        `Après ces paroles, tandis que les apôtres le regardaient,
+        il s'éleva et une nuée vint le soustraire à leurs yeux.
+        Et comme ils fixaient le ciel où Jésus s'en allait,
+        voici que, devant eux, se tenait deux hommes en vêtements blancs,
+        qui leur dirent : "Galiléens, pourquoi restez-vous là à regarder vers le ciel ?
+        Ce Jésus qui a été enlevé au ciel d'auprès de vous, viendra de la même manière
+        que vous l'avez vu s'en aller vers le ciel. (Ac 1, 9-11)`,
+        // 3
+        `Soudain, un bruit survint du ciel comme un violent coup de vent.
+        La maison où ils étaient assis en fut remplie toute entière.
+        Alors leur apparurent des langues qu'on aurait dites de feu, qui se partageaient,
+        et il s'en posa une sur chacun d'eux.
+        Tous furent remplis d’Esprit Saint. 
         Ils se mirent à parler en d’autres langues, 
-        et chacun s’exprimait selon le don de l’Esprit. (Ac 2, 4)`,
+        et chacun s’exprimait selon le don de l’Esprit. (Ac 2, 2-4)`,
         // 4
         `"Le Puissant fit pour moi des merveilles. Saint est son nom !" (Lc 1, 49)`,
         // 5
         `Un grand signe apparut dans le ciel :
-        une Femme, ayant le soleil pour manteau, 
-        la lune sous les pieds, et sur la tête, 
-        une couronne de 12 étoiles. (Ap 12, 1)`,
+        une Femme, ayant le soleil pour manteau, la lune sous les pieds, 
+        et sur la tête, une couronne de 12 étoiles. (Ap 12, 1)`,
     ]
 };
