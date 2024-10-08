f57dacc0818d8c46998f15b73021fa8286521646
Lucien HAMM <lucien.hamm@gmail.com>
2024-10-08 18:22:47 +0200

    MAJ R C-Liberation

--- public/js/chapelets/ch-liberation.js
+++ public/js/chapelets/ch-liberation.js
@@ -4,9 +4,35 @@ const selRondes = $("#sel_rondes");
 
 const txtIntention = $("#txt_intention");
 
-const btnPremier = $("#dz_grains_1");
+const divTrio = $("#trio");
+const divDizaines = $("#dizaines");
+
+const trioPremier = $("#base_trio_1");
+const dzPremier = $("#dz_grains_1");
+
+// Variables
+
+let index = 0;
 
 /* Fonctions */
+
+resetAll();
+
+// Changement de bloc
+
+function setBlock()
+{
+    index = selRondes.selectedIndex;
+
+    if (index == 0) {
+        switchBlocks(divTrio, divDizaines);
+    } else {
+        switchBlocks(divDizaines, divTrio);
+    }
+
+    checkButton(trioPremier);
+    checkButton(dzPremier);
+}
 
 // Réinitialisation du chapelet
 
@@ -15,5 +41,5 @@ function resetAll()
     selRondes.selectedIndex = 0;
     txtIntention.value = "";
 
-    checkFirst();
+    setBlock();
 }
