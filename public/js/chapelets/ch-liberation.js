d0b7b9e05726297e19ffabcf71c7532261f9a51e
Lucien HAMM <lucien.hamm@gmail.com>
2024-10-09 13:50:41 +0200

    MAJ R C-Liberation > Intention

--- public/js/chapelets/ch-liberation.js
+++ public/js/chapelets/ch-liberation.js
@@ -4,6 +4,14 @@ const selRondes = $("#sel_rondes");
 
 const txtIntention = $("#txt_intention");
 
+const spanPrefixe = $("#prefixe");
+const spanSuffixe = $("#suffixe");
+
+const spanPronom = $("#pronom");
+const spanVerbe = $("#verbe");
+
+const lignes = $all("li .gras");
+
 const divTrio = $("#trio");
 const divDizaines = $("#dizaines");
 
@@ -13,9 +21,13 @@ const dzPremier = $("#dz_grains_1");
 // Variables
 
 let index = 0;
+let numLigne = 0;
+
+let nom = "";
 
 /* Fonctions */
 
+//hideAll(maskable);
 resetAll();
 
 // Changement de bloc
@@ -34,6 +46,53 @@ function setBlock()
     checkButton(dzPremier);
 }
 
+// MAJ du verset
+
+function setVerset(prefixe, suffixe, pronom, verbe)
+{
+    spanPrefixe.textContent = prefixe;
+    spanSuffixe.textContent = suffixe;
+
+    spanPronom.textContent = pronom;
+    spanVerbe.textContent = verbe;
+}
+
+// MAJ du texte
+
+function updateText()
+{
+    nom = txtIntention.value;
+
+    if (nom) {
+        // Verset
+
+        setVerset("", nom, nom, "sera");
+
+        // Lignes
+
+        for (let ligne of lignes) {
+            ligne.textContent = " " + nom;
+        }
+    } else {
+        // Verset
+
+        setVerset("me", "", "je", "serai");
+
+        // Lignes
+
+        numLigne = 0;
+        nom = "moi";
+
+        for (let ligne of lignes) {
+            if (numLigne > 0)   nom = "-moi";
+
+            ligne.textContent = nom;
+            
+            numLigne++;
+        }
+    }
+}
+
 // Réinitialisation du chapelet
 
 function resetAll()
@@ -42,4 +101,5 @@ function resetAll()
     txtIntention.value = "";
 
     setBlock();
+    updateText();
 }
