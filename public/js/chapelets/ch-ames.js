6673385c2e929f8f9949c6afefe017c9d9ea808d
Lucien HAMM <lucien.hamm@gmail.com>
2024-12-26 15:12:07 +0100

    MAJ R C-Ames > Niveaux

--- public/js/chapelets/ch-ames.js
+++ public/js/chapelets/ch-ames.js
@@ -2,47 +2,76 @@
 
 const basePremier = $("#base_trio_1");
 const dzPremier = $("#dz_trio_1");
+const trzPremier = $("#trz_niveaux_1");
 
 const divMedaille = $("#dz_medaille");
 const divRepos = $("#dz_repos");
 
-const selNiveaux = $("#sel_niveaux");
+const spanNiveau = $("#niveau");
+
+const treizaine = $name("trz_niveaux");
 
 const maskable = $all(".maskable");
 
 // Variables
 
+const niveaux = [
+    "1. Crainte de Dieu", "2. Charité", "3. Foi", "4. Espérance",
+    "5. Obéissance", "6. Patience", "7. Piété", "8. Chasteté",
+    "9. Prudence", "10. Force", "11. Tempérance", "12. Pureté",
+    "13. Justice",
+];
+
 let index = 0;
+let niveau = "Médaille";
+
+// Evénements
+
+for (let radioBtn of treizaine)
+{   
+    radioBtn.addEventListener("click", () => setBlock(radioBtn.value));
+}
 
 /* Fonctions */
 
-setBlock();
+resetAll();
 
 // Debug
 
 //hideAll(maskable);
 
+// MAJ du niveau
+
+function setLevel(text)
+{
+    spanNiveau.innerText = text;
+}
+
 // MAJ du bloc et réinitialisation des boutons radio
 
-function setBlock()
+function setBlock(index)
 {
-    index = selNiveaux.selectedIndex;
-
     if (index == 0) {
         switchBlocks(divMedaille, divRepos);
+
+        setLevel("Médaille");
     } else {
+        niveau = niveaux[index - 1];
+
         switchBlocks(divRepos, divMedaille);
+
+        setLevel(niveau);
     }
 
     checkButton(basePremier);
     checkButton(dzPremier);
 }
 
-// Réinitialiser du chapelet
+// Réinitialisation du chapelet
 
 function resetAll()
 {
-    selNiveaux.selectedIndex = 0;
+    setBlock(0);
 
-    setBlock();
+    checkButton(trzPremier);
 }
