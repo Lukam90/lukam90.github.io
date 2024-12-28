7c97d213824c112240793a8a13f8c53d9a1f0c53
Lucien HAMM <lucien.hamm@gmail.com>
2024-12-28 20:01:15 +0100

    MAJ R Dizaines Min

--- compile.py
+++ compile.py
@@ -32,9 +32,9 @@ prieres = ["chapelet", "dizaines", "ex-trentain", "litanies"]
 #convert_jinja_list("prieres", prieres)
 
 convert_jinja_single("prieres/chapelet")
-#convert_jinja_single("prieres/dizaines")
+convert_jinja_single("prieres/dizaines")
 #convert_jinja_single("prieres/ex-trentain")
-convert_jinja_single("prieres/litanies")
+#convert_jinja_single("prieres/litanies")
 
 ## Exercices
 
