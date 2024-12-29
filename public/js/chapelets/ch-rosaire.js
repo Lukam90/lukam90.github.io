77ab8b294c731d9a6cad20f57efa83c4d1a5834c
Lucien HAMM <lucien.hamm@gmail.com>
2024-12-29 15:44:04 +0100

    MAJ R C-Ames V1

---
+++ public/js/chapelets/ch-rosaire.js
@@ -0,0 +1,73 @@
+// Données
+
+const chapelets = {
+    joyeux : ch_joyeux,
+    lumineux : ch_lumineux,
+    douloureux : ch_douloureux,
+    glorieux : ch_glorieux
+};
+
+// Eléments
+
+const selType = $("#sel_type");
+const selMysteres = $("#sel_mysteres");
+
+const spanFruit = $("#fruit");
+
+const btnPremier = $("#dz_grains_1");
+
+// Variables
+
+let chapelet, mysteres, fruits;
+
+let index = 0;
+let type = "joyeux";
+let fruit = "";
+
+let estOptionnel = false;
+
+initData("joyeux");
+
+//debugHideAll();
+
+// Initialisation des données
+
+function initData(type)
+{
+    chapelet = chapelets[type];
+
+    mysteres = chapelet["mysteres"];
+    fruits = chapelet["fruits"];
+}
+
+// MAJ des mystères d'un chapelet selon le type
+
+function majMysteres() 
+{
+    index = 0;
+
+    type = selType.value;
+
+    initData(type);
+
+    for (let optMystere of selMysteres.children) {
+        optMystere.innerText = mysteres[index++];
+    }
+
+    selMysteres.selectedIndex = 0;
+
+    majInfos();
+}
+
+// MAJ des données selon le mystère sélectionné
+
+function majInfos() 
+{
+    index = selMysteres.selectedIndex;
+
+    fruit = fruits[index];
+
+    spanFruit.textContent = fruit;
+
+    checkFirst();
+}
