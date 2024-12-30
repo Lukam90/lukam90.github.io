f66416ceb2440118c5120b107c752e07d4c2a942
Lucien HAMM <lucien.hamm@gmail.com>
2024-12-30 14:49:10 +0100

    MAJ R Tri & Chapelet

--- compile.py
+++ compile.py
@@ -27,16 +27,11 @@ profil = ["cv-imprime", "portfolio"]
 
 ## Prières
 
-prieres = [
-    "chapelet", "ch-ames", "ch-liberation",
-    "dizaines", "ex-trentain", "litanies"
-]
+prieres = ["chapelet", "dizaines", "ex-trentain", "litanies"]
 
-#convert_jinja_list("prieres", prieres)
+convert_jinja_list("prieres", prieres)
 
 #convert_jinja_single("prieres/chapelet")
-#convert_jinja_single("prieres/ch-ames")
-convert_jinja_single("prieres/ch-liberation")
 #convert_jinja_single("prieres/dizaines")
 #convert_jinja_single("prieres/ex-trentain")
 #convert_jinja_single("prieres/litanies")
