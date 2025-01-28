54aa0b4d69f01079a9e0347f5a0d134610b8d3f1
Lucien HAMM <lucien.hamm@gmail.com>
2025-01-28 10:36:19 +0100

    MAJ R C-Ames

--- compile.py
+++ compile.py
@@ -27,12 +27,14 @@ profil = ["cv-imprime", "portfolio"]
 
 ## Prières
 
-prieres = ["ex-base", "rosaire", "litanies"]
+prieres = ["ex-base", "chapelet", "ch-ames", "ch-mercis", "litanies"]
 
 #convert_jinja_list("prieres", prieres)
 
 #convert_jinja_single("prieres/ex-base")
-#convert_jinja_single("prieres/rosaire")
+convert_jinja_single("prieres/chapelet")
+convert_jinja_single("prieres/ch-ames")
+#convert_jinja_single("prieres/ch-mercis")
 #convert_jinja_single("prieres/litanies")
 
 ## Exercices
