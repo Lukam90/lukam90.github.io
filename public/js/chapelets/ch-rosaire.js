4739de6838155d0566022ab769e9707be5cda5b6
Lucien HAMM <lucien.hamm@gmail.com>
2025-02-03 17:51:01 +0100

    MAJ C-Rosaire > Dizaine

--- public/js/chapelets/ch-rosaire.js
+++ public/js/chapelets/ch-rosaire.js
@@ -1,8 +1,88 @@
+/* Données texte */
+
+const rosaire = {
+    joyeux : [
+        "1. L'annonciation",
+        "2. La visitation",
+        "3. la naissance de Jésus",
+        "4. la présentation de Jésus au temple de Jérusalem",
+        "5. le recouvrement de Jésus au temple de Jérusalem"
+    ],
+    lumineux : [
+        "1. Le baptême de Jésus",
+        "2. Les noces de Cana",
+        "3. L'annonce du Royaume de Dieu",
+        "4. La transfiguration au Mont Thabor",
+        "5. L'institution de l'eucharistie"
+    ],
+    douloureux : [
+        "1. L'agonie au jardin de Gethsémani",
+        "2. La flagellation",
+        "3. Le couronnement d'épines",
+        "4. Le portement de croix jusqu'au Golgotha",
+        "5. La crucifixion"
+    ],
+    glorieux : [
+        "1. La résurrection de Jésus",
+        "2. L'ascension",
+        "3. La Pentecôte",
+        "4. L'Assomption de Marie",
+        "5. Le couronnement de Marie"
+    ]
+};
+
 /* Eléments */
 
-const btnPremier = $("#j1");
+const selType = $("#sel_type");
+
+const mGrains = $name("mysteres");
+const mLabels = $all("label");
+
+const dzPremier = $("#dz_grains_1");
+
+/* Variables */
+
+let type = "joyeux";
+let mysteres = [];
+
+/* Evénement de clic */
+
+for (let radioBtn of mGrains)
+{
+    radioBtn.addEventListener("click", () => resetDizaine());
+}
 
 /* Fonctions */
+
+// Sélection d'un type
+
+function selectType()
+{
+    type = selType.value;
+
+    mysteres = rosaire[type];
+
+    for (let i = 0 ; i < 5 ; i++)
+    {
+        mLabels[i].textContent = mysteres[i];
+    }
+
+    selectMystere(0);
+}
+
+// Sélection d'un mystère (raccourci)
+
+function selectMystere(index)
+{
+    mGrains[index].click();
+}
+
+// Réinitialisation de la dizaine
+
+function resetDizaine()
+{
+    dzPremier.click();
+}
 
 // Réinitialisation du rosaire
 
@@ -10,16 +90,17 @@ function resetAll()
 {
     goTo("#");
 
-    btnPremier.click();
+    selType.selectedIndex = 0;
+
+    selectType();
 }
 
 /* Raccourcis */
 
 document.addEventListener("keydown", e => {
-    if (e.key == "0")   resetAll();
+    numKey = parseInt(e.key);
 
-    if (e.key == "1")   goTo("#m_joyeux");
-    if (e.key == "2")   goTo("#m_lumineux");
-    if (e.key == "3")   goTo("#m_douloureux");
-    if (e.key == "4")   goTo("#m_glorieux");
+    if (numKey == 0)   resetAll();
+
+    if (numKey >= 1 && numKey <= 5)   selectMystere(numKey - 1);
 });
