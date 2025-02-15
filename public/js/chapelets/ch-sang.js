41321e3f54f43edfac13d5b4b5fc5fcc59f9af4e
Lucien HAMM <lucien.hamm@gmail.com>
2025-02-15 20:53:23 +0100

    MAJ R C-Sang

---
+++ public/js/chapelets/ch-sang.js
@@ -0,0 +1,67 @@
+/* Eléments */
+
+const buttons = $name("mysteres");
+
+const divPremiers = $("#premiers");
+const divDernier = $("#dernier");
+
+const qntPremier = $("#quintes_1");
+const basePremier = $("#base_trio_1");
+const finPremier = $("#fin_trio_1");
+
+/* Variables */
+
+let numKey = 0;
+
+/* Evénements de clic */
+
+for (let i = 0 ; i < 7 ; i++)
+{
+    buttons[i].addEventListener("click", () => setBlock(i));
+}
+
+/* Fonctions */
+
+resetAll();
+
+// MAJ du bloc
+
+function setBlock(index)
+{
+    buttons[index].click();
+
+    if (index < 6) {
+        switchBlocks(divPremiers, divDernier);
+
+        qntPremier.click();
+    } else {
+        switchBlocks(divDernier, divPremiers);
+
+        finPremier.click();
+    }
+}
+
+// Réinitialisation du chapelet
+
+function resetAll()
+{
+    goTo("#");
+
+    setBlock(0);
+
+    basePremier.click();
+}
+
+/* Raccourcis */
+
+document.addEventListener("keydown", e => {
+    numKey = parseInt(e.key);
+
+    if (numKey == 0)   resetAll();
+
+    if (numKey >= 1 && numKey <= 7) {
+        goTo("#mysteres");
+
+        setBlock(numKey - 1);
+    }
+});
