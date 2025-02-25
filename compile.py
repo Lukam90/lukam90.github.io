78928f470916f746f8d13bcccf19a6828e23a066
Lucien HAMM <lucien.hamm@gmail.com>
2025-02-25 09:41:04 +0100

    MAJ R C-Ames ~ Rosaire

--- compile.py
+++ compile.py
@@ -36,7 +36,7 @@ convert_jinja_single("prieres/ex-base")
 #convert_jinja_single("prieres/devotion")
 #convert_jinja_single("prieres/misericorde")
 convert_jinja_single("prieres/ames")
-convert_jinja_single("prieres/plaies")
+#convert_jinja_single("prieres/plaies")
 
 dpath = "prieres/divers/"
 
--- public/js/chapelets/ch-ames.js
+++ public/js/chapelets/ch-ames.js
@@ -1,27 +1,8 @@
-/* Eléments */
-
-const grains = $name("dz_grains");
-
-let grain;
-
-/* Fonctions */
-
-// Sélection d'un grain
-
-function selectNum(index)
-{
-    grain = grains[index];
-
-    grain.click();
-    grain.select();
-}
-
 /* Raccourcis */
 
 document.addEventListener("keydown", e => {
-    numKey = parseInt(e.key);
-
-    if (numKey >= 0 && numKey <= 9) selectNum(numKey);
-    
-    if (e.key == "f")   selectNum(10);
+    if (isMajKey(e)) {
+        if (e.key == "A")   goTo("#dz_ames");
+        if (e.key == "C")   goTo("#dz_coeurs");
+    }
 });
