5d4f2ddddc68d48463266c1433ac0994d359a642
Lucien HAMM <lucien.hamm@gmail.com>
2025-02-27 14:52:19 +0100

    MAJ R C-Sang V1

--- compile.py
+++ compile.py
@@ -27,15 +27,16 @@ profil = ["cv-imprime", "portfolio"]
 
 ## Prières
 
-prieres = ["ex-base", "rosaire", "misericorde", "plaies", "mercis"]
+prieres = ["ex-base", "rosaire", "misericorde", "plaies", "sang", "mercis"]
 
 #convert_jinja_list("prieres", prieres)
 
 convert_jinja_single("prieres/ex-base")
 #convert_jinja_single("prieres/rosaire")
 #convert_jinja_single("prieres/misericorde")
-convert_jinja_single("prieres/plaies")
-convert_jinja_single("prieres/mercis")
+#convert_jinja_single("prieres/plaies")
+convert_jinja_single("prieres/sang")
+#convert_jinja_single("prieres/mercis")
 
 dpath = "prieres/divers/"
 
