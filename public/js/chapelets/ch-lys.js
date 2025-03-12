36f5239a78a7321c34299d1d4790942c5d0967ac
Lucien HAMM <lucien.hamm@gmail.com>
2025-03-12 21:55:30 +0100

    MAJ R C-Lys

--- public/js/chapelets/ch-lys.js
+++ public/js/chapelets/ch-lys.js
@@ -2,32 +2,25 @@
 
 const rosaire = {
     joyeux : [
-        "1. L'annonciation",
-        "2. La visitation",
-        "3. la naissance de Jésus",
-        "4. la présentation de Jésus au temple de Jérusalem",
-        "5. le recouvrement de Jésus au temple de Jérusalem"
-    ],
-    lumineux : [
-        "1. Le baptême de Jésus",
-        "2. Les noces de Cana",
-        "3. L'annonce du Royaume de Dieu",
-        "4. La transfiguration au Mont Thabor",
-        "5. L'institution de l'eucharistie"
+        "1. L'Annonciation à Joseph",
+        "2. La naissance de Jésus ",
+        "3. La circoncision et l’attribution du nom de Jésus",
+        "4. La présentation au temple",
+        "5. Le recouvrement"
     ],
     douloureux : [
-        "1. L'agonie au jardin de Gethsémani",
-        "2. La flagellation",
-        "3. Le couronnement d'épines",
-        "4. Le portement de croix jusqu'au Golgotha",
-        "5. La crucifixion"
+        "1. Joseph envisage de répudier Marie dans le secret",
+        "2. L'absence de place à l'auberge de Bethléem",
+        "3. La fuite en Égypte",
+        "4. Jésus perdu à Jérusalem",
+        "5. La mort de Joseph"
     ],
     glorieux : [
-        "1. La résurrection de Jésus",
-        "2. L'ascension de Jésus",
-        "3. La Pentecôte",
-        "4. L'Assomption de Marie",
-        "5. Le couronnement de Marie"
+        "1. La glorification de Joseph",
+        "2. St Joseph, protecteur de l’Église Universelle",
+        "3. St Joseph, patron des familles",
+        "4. St Joseph, patron de la bonne mort",
+        "5. St Joseph, protecteur des mourants et des souffrants"
     ]
 };
 
