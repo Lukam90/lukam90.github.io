8e926f3e67a0eca8426731ecc986cfa16a9db7f7
Lucien HAMM <lucien.hamm@gmail.com>
2025-03-15 21:58:45 +0100

    MAJ R C-Liberation > Simple

--- public/js/chapelets/ch-liberation.js
+++ public/js/chapelets/ch-liberation.js
@@ -2,23 +2,7 @@
 
 const rondes = $name("dz_rondes");
 
-const spanNombre = $("#nombre");
-
 let radioBtn;
-
-/* Evénement de clic */
-
-for (let index = 0 ; index < 5 ; index++)
-{
-    rondes[index].addEventListener("click", () => majNombre(index));
-}
-
-// MAJ du nombre
-
-function majNombre(index)
-{
-    spanNombre.textContent = (index == 0) ? "3" : "10";
-}
 
 /* Fonctions */
 
@@ -37,15 +21,9 @@ function selectRonde(index)
 document.addEventListener("keydown", e => {
     let numKey = parseInt(e.key);
 
-    if (numKey >= 0 && numKey <= 5) {
+    if (numKey >= 1 && numKey <= 5) {
         goTo("#rondes");
 
-        selectRonde(numKey);
-    }
-
-    if (isMajKey(e)) {
-        if (e.key == "B")   goTo("#base");
-        if (e.key == "R")   goTo("#rondes");
-        if (e.key == "F")   goTo("#fin");
+        selectRonde(numKey - 1);
     }
 });
