33d6802ca59113e121a5d46c952d03dea40959be
Lucien HAMM <lucien.hamm@gmail.com>
2025-03-16 14:45:02 +0100

    MAJ R C-Ames

--- public/js/chapelets/ch-ames.js
+++ public/js/chapelets/ch-ames.js
@@ -1,8 +1,55 @@
+/* Eléments */
+
+const grains = $name("dz_rondes");
+
+const basePremier = $("#base_trio_1");
+const btnPremier = $("#dz_grains_1");
+const finPremier = $("#fin_trio_1");
+
+let grain;
+
+/* Fonctions */
+
+// Réinitialisation des boutons
+
+function resetButtons()
+{
+    basePremier.click();
+    btnPremier.click();
+    finPremier.click();
+}
+
+// Sélection d'une ronde (raccourci)
+
+function selectRonde(index)
+{
+    goTo("#rondes");
+
+    checkFirst();
+
+    grain = grains[index];
+
+    grain.select();
+    grain.click();
+}
+
+// Réinitialisation du chapelet
+
+function resetAll()
+{
+    resetButtons();
+
+    selectRonde(0);
+
+    goTo("#");
+}
+
 /* Raccourcis */
 
 document.addEventListener("keydown", e => {
-    if (isMajKey(e)) {
-        if (e.key == "A")   goTo("#dz_ames");
-        if (e.key == "C")   goTo("#dz_coeurs");
-    }
+    numKey = parseInt(e.key);
+
+    if (numKey == 0)    resetAll();
+
+    if (numKey >= 1 && numKey <= 5) selectRonde(numKey - 1);
 });
