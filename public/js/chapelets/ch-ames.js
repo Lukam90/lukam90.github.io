820113265fe8bd2607de5fbc53fe6b8c108e2f31
Lucien HAMM <lucien.hamm@gmail.com>
2025-03-20 22:21:47 +0100

    MAJ R C-Ames V2

--- public/js/chapelets/ch-ames.js
+++ public/js/chapelets/ch-ames.js
@@ -1,8 +1,18 @@
 /* Raccourcis */
 
 document.addEventListener("keydown", e => {
+    let numKey = parseInt(e.key);
+
+    if (numKey == 0)   resetDizaine();
+
+    if (numKey >= 1 && numKey <= 5) {
+        goTo("#mysteres");
+
+        selectMystere(numKey - 1);
+    }
+
     if (isMajKey(e)) {
-        if (e.key == "A")   goTo("#dz_ames");
         if (e.key == "C")   goTo("#dz_coeurs");
+        if (e.key == "A")   goTo("#dz_ames");
     }
 });
