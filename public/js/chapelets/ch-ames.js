14ff7c335175f493b5f07ba0e383352fc10ee674
Lucien HAMM <lucien.hamm@gmail.com>
2025-03-20 10:39:35 +0100

    MAJ R C-Ames > Rosaire

--- public/js/chapelets/ch-ames.js
+++ public/js/chapelets/ch-ames.js
@@ -1,53 +1,8 @@
-/* Eléments */
-
-const grains = $name("dz_rondes");
-
-const basePremier = $("#base_trio_1");
-const btnPremier = $("#dz_grains_1");
-
-let grain;
-
-/* Fonctions */
-
-// Sélection d'une ronde (raccourci)
-
-function selectRonde(index)
-{
-    goTo("#rondes");
-
-    checkFirst();
-
-    grain = grains[index];
-
-    grain.select();
-    grain.click();
-}
-
-// Réinitialisation du chapelet
-
-function resetAll()
-{
-    basePremier.click();
-
-    selectRonde(0);
-
-    goTo("#");
-}
-
 /* Raccourcis */
 
 document.addEventListener("keydown", e => {
-    numKey = parseInt(e.key);
-
-    if (numKey == 0)    resetAll();
-
-    if (numKey >= 1 && numKey <= 5) selectRonde(numKey - 1);
-
     if (isMajKey(e)) {
-        if (e.key == "P" || e.key == "R")   goTo("#rondes");
-        
         if (e.key == "A")   goTo("#dz_ames");
         if (e.key == "C")   goTo("#dz_coeurs");
-        if (e.key == "G")   goTo("#dz_gloria");
     }
 });
