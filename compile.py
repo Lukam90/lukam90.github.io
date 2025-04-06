d34e05c490eaf6c9d0b75dc63cfd9847c279107a
Lucien HAMM <lucien.hamm@gmail.com>
2025-04-06 19:04:56 +0200

    MAJ R C-Devotion & C-Plaies

--- compile.py
+++ compile.py
@@ -28,25 +28,21 @@ profil = ["cv-imprime", "portfolio"]
 ## Prières
 
 prieres = [
-    "ex-base", "chapelet", "abandon", "reparation", 
-    "anges", "sang", "misericorde", "plaies",
-    "impossible", "vertus", "dzames", "lys"
+    "ex-base", 
+    "chapelet", "lys", "misericorde",
+    "sang", "plaies", "devotion",
+    
 ]
 
 #convert_jinja_list("prieres", prieres)
 
 #convert_jinja_single("prieres/ex-base")
 #convert_jinja_single("prieres/chapelet")
-#convert_jinja_single("prieres/abandon")
-#convert_jinja_single("prieres/reparation")
-#convert_jinja_single("prieres/anges")
-#convert_jinja_single("prieres/sang")
-#convert_jinja_single("prieres/misericorde")
-convert_jinja_single("prieres/plaies")
-#convert_jinja_single("prieres/impossible")
-#convert_jinja_single("prieres/vertus")
-convert_jinja_single("prieres/dzames")
 #convert_jinja_single("prieres/lys")
+#convert_jinja_single("prieres/misericorde")
+#convert_jinja_single("prieres/sang")
+convert_jinja_single("prieres/plaies")
+convert_jinja_single("prieres/devotion")
 
 ## Exercices
 
