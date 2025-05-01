20242b3a9dda8e90fa5e644f0e82eceb4bff9ce2
Lucien HAMM <lucien.hamm@gmail.com>
2025-05-01 21:44:48 +0200

    MAJ R C-Devotion

--- compile.py
+++ compile.py
@@ -28,18 +28,18 @@ profil = ["cv-imprime", "portfolio"]
 ## Chapelets
 
 chapelets = [
-    "ex-base", "rosaire", "dzames",
-    "sang", "misericorde", "plaies",
+    "ex-base", "rosaire", "plaies",
+    "devotion", "sang", "misericorde",
 ]
 
 #convert_jinja_list("chapelets", chapelets)
 
 #convert_jinja_single("chapelets/ex-base")
 convert_jinja_single("chapelets/rosaire")
-convert_jinja_single("chapelets/dzames")
+convert_jinja_single("chapelets/plaies")
+convert_jinja_single("chapelets/devotion")
 #convert_jinja_single("chapelets/sang")
 #convert_jinja_single("chapelets/misericorde")
-#convert_jinja_single("chapelets/plaies")
 
 ## Litanies
 
