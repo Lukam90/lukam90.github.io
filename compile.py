325076ced1de2379ad0a26f146a2eb5eecd64c34
Lucien HAMM <lucien.hamm@gmail.com>
2025-05-03 11:54:02 +0200

    MAJ R C-Rosaire

--- compile.py
+++ compile.py
@@ -35,9 +35,9 @@ chapelets = [
 #convert_jinja_list("chapelets", chapelets)
 
 #convert_jinja_single("chapelets/ex-base")
-#convert_jinja_single("chapelets/rosaire")
-convert_jinja_single("chapelets/plaies")
-convert_jinja_single("chapelets/devotion")
+convert_jinja_single("chapelets/rosaire")
+#convert_jinja_single("chapelets/plaies")
+#convert_jinja_single("chapelets/devotion")
 #convert_jinja_single("chapelets/sang")
 #convert_jinja_single("chapelets/misericorde")
 
