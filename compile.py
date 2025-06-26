a8ede20f16e8ebd892a459b3dd358634e0a134b6
Lucien HAMM <lucien.hamm@gmail.com>
2025-06-26 21:06:52 +0200

    MAJ R C-Ames

--- compile.py
+++ compile.py
@@ -27,7 +27,7 @@ profil = ["cv-imprime", "portfolio"]
 
 ## Chapelets
 
-chapelets = ["ex-base", "rosaire", "plaies", "sang", "misericorde", "mercis"]
+chapelets = ["ex-base", "rosaire", "plaies", "sang", "misericorde", "ames", "mercis"]
 
 #convert_jinja_list("chapelets", chapelets)
 
@@ -36,6 +36,7 @@ chapelets = ["ex-base", "rosaire", "plaies", "sang", "misericorde", "mercis"]
 convert_jinja_single("chapelets/plaies")
 #convert_jinja_single("chapelets/sang")
 #convert_jinja_single("chapelets/misericorde")
+convert_jinja_single("chapelets/ames")
 convert_jinja_single("chapelets/mercis")
 
 ## Litanies
