707dff56d2264828bb520f1767c249d63fda0b0f
Lucien HAMM <lucien.hamm@gmail.com>
2025-08-19 21:18:54 +0200

    MAJ Nv Tri + Ex Video

--- compile.py
+++ compile.py
@@ -19,39 +19,15 @@ index = convert_json_data("index")
 convert_jinja_single("index", index)
 convert_jinja_single("test")
 
-## Profil
-
-profil = ["cv-imprime", "portfolio"]
-
-#convert_jinja_list("profil", profil)
-
 ## Chapelets
 
-chapelets = ["ex-base", "rosaire", "plaies", "sang", "misericorde", "mercis"]
+chapelets = ["ex-base", "rosaire", "misericorde"]
 
 convert_jinja_list("chapelets", chapelets)
 
 #convert_jinja_single("chapelets/ex-base")
 #convert_jinja_single("chapelets/rosaire")
-convert_jinja_single("chapelets/plaies")
-#convert_jinja_single("chapelets/sang")
 #convert_jinja_single("chapelets/misericorde")
-convert_jinja_single("chapelets/mercis")
-
-## Litanies
-
-litanies = ["lorette", "lt-sang", "lt-joseph", "lt-charbel"]
-
-convert_jinja_list("litanies", litanies)
-
-#convert_jinja_single("litanies/lorette")
-#convert_jinja_single("litanies/lt-sang")
-#convert_jinja_single("litanies/lt-joseph")
-#convert_jinja_single("litanies/lt-charbel")
-
-## Spécial
-
-convert_jinja_single("special/ames")
 
 ## Exercices
 
