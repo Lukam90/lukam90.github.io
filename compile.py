b057e9db3fcf4c46e3a0e7c0e07d4d2121caa50e
Lucien HAMM <lucien.hamm@gmail.com>
2025-10-12 22:52:24 +0200

    MAJ C-Impossible

--- compile.py
+++ compile.py
@@ -21,7 +21,7 @@ convert_jinja_single("test")
 
 ## Chapelets
 
-chapelets = ["ex-base", "litanies", "rosaire", "misericorde", "vertus"]
+chapelets = ["ex-base", "litanies", "rosaire", "misericorde", "impossible"]
 
 #convert_jinja_list("chapelets", chapelets)
 
@@ -29,7 +29,7 @@ chapelets = ["ex-base", "litanies", "rosaire", "misericorde", "vertus"]
 convert_jinja_single("chapelets/litanies")
 #convert_jinja_single("chapelets/rosaire")
 convert_jinja_single("chapelets/misericorde")
-convert_jinja_single("chapelets/vertus")
+convert_jinja_single("chapelets/impossible")
 
 ## Exercices
 
