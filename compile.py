7aad3b5f7ce183a0197e0bbe1e15dffc43155a25
Lucien HAMM <lucien.hamm@gmail.com>
2025-10-21 21:25:08 +0200

    MAJ R C-Larmes

--- compile.py
+++ compile.py
@@ -21,15 +21,16 @@ convert_jinja_single("test")
 
 ## Chapelets
 
-chapelets = ["ex-base", "rosaire", "misericorde", "cplaies", "vertus"]
+chapelets = ["ex-base", "rosaire", "misericorde", "cplaies", "vertus", "larmes"]
 
 #convert_jinja_list("chapelets", chapelets)
 
 #convert_jinja_single("chapelets/ex-base")
 #convert_jinja_single("chapelets/rosaire")
 #convert_jinja_single("chapelets/misericorde")
-convert_jinja_single("chapelets/cplaies")
-convert_jinja_single("chapelets/vertus")
+#convert_jinja_single("chapelets/cplaies")
+#convert_jinja_single("chapelets/vertus")
+convert_jinja_single("chapelets/larmes")
 
 ## Exercices
 
